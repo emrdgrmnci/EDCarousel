34a8c2a30be87140496a465fbe357c1d95e1aec4
Emre <emre.degirmenci@sisal.com>
2023-05-11 12:09:19 +0300

    Add framework

---
+++ EDCarousel/EDCarousel/EDCarousel.h
@@ -0,0 +1,18 @@
+//
+//  EDCarousel.h
+//  EDCarousel
+//
+//  Created by Degirmenci Emre on 11.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EDCarousel.
+FOUNDATION_EXPORT double EDCarouselVersionNumber;
+
+//! Project version string for EDCarousel.
+FOUNDATION_EXPORT const unsigned char EDCarouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EDCarousel/PublicHeader.h>
+
+
